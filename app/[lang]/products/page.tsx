d582b6c74f2dd123b57460bddea46375b9df62c6
KiloMystery <kilomystery2025@gmail.com>
2026-02-16 08:30:05 +0100

    fix

--- app/[lang]/products/page.tsx
+++ app/[lang]/products/page.tsx
@@ -5,11 +5,13 @@
 
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import Image from "next/image";
-import Header from "./../components/Header";
-import Footer from "./../components/Footer";
-import { useCart } from "./../components/cart/CartProvider";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import { useCart } from "../../components/cart/CartProvider";
+import SectionInsideBox from "../../components/SectionInsideBox";
+
 import { Lang, normalizeLang } from "@/i18n/lang";
-import SectionInsideBox from "./../components/SectionInsideBox";
+
 import { trackAddToCart, trackViewContent, trackViewItemList } from "@/app/lib/tracking";
 import type { KMCartItem } from "@/app/lib/ga";
 
